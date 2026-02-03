f0fe024ec83b17c056925e548ac0eb6fc7fb6820
JetnipitKaewsup <Jetnipit.Kaewsup@gmail.com>
2026-02-03 19:41:36 +0700

    test add firestore

--- app/src/main/java/com/example/petbuddyproject/MainActivity.kt
+++ app/src/main/java/com/example/petbuddyproject/MainActivity.kt
@@ -5,8 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+    private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private val docRef: CollectionReference = db.collection("Notebook")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+        val note = mutableMapOf<String, Any>()
+        note["name"] = "Lookkaew"
+        note["password"] = "5555555567"
+
+        docRef.add(note)
     }
 }
